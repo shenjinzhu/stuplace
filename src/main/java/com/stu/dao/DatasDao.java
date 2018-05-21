b6849a473ac9aebcffad5661424787812e15854a
admin <admin@DESKTOP-27ON3SN>
2018-05-21 18:31:09 +0800

    测试修改，未修复BUG

---
+++ src/main/java/com/stu/dao/DatasDao.java
@@ -0,0 +1,11 @@
+package com.stu.dao;
+
+import com.stu.bean.Datas;
+
+public interface DatasDao {
+
+	void insertDatas(Datas data);
+
+	// Person selectPersonById(@Param("personId") Integer personId);
+
+}
--- src/test/java/studyPlace/DatasDaoTest.java
+++ src/test/java/studyPlace/DatasDaoTest.java
@@ -1,6 +1,5 @@
 package studyPlace;
 
-
 import java.sql.Date;
 
 import org.junit.Test;
@@ -10,18 +9,18 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.stu.bean.Datas;
-import com.stu.dao.DatasDaoImpl;
+import com.stu.dao.DatasDao;
 
-@RunWith(SpringJUnit4ClassRunner.class) //使用junit4进行测试  
-@ContextConfiguration(locations={"classpath:applicationContext.xml"}) //加载配置文件   
+@RunWith(SpringJUnit4ClassRunner.class) // 使用junit4进行测试
+@ContextConfiguration(locations = { "classpath:applicationContext.xml" }) // 加载配置文件
 public class DatasDaoTest {
 
 	@Autowired
-	private DatasDaoImpl dao;
-	
-//	@Test
+	private DatasDao dao;
+
+	@Test
 	public void testSave() {
-		Datas data=new Datas();
+		Datas data = new Datas();
 		data.setTitle("12");
 		data.setAuthor("12");
 		data.setCai(11);
@@ -34,12 +33,12 @@ public class DatasDaoTest {
 		data.setUp_time(new Date(System.currentTimeMillis()));
 		data.setUseful(true);
 		data.setZan(10);
-		dao.insert(data);
+		dao.insertDatas(data);
 	}
-	
-	@Test
+
+	// @Test
 	public void testDelete() {
-		dao.delete("1441");
+		// dao.delete("1441");
 	}
-	
+
 }
