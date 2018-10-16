d1cde51d8f47361ffbf1c91914a3f2b3af2cb9b8
shenjinzhu123 <949131080@qq.com>
2018-10-16 16:42:52 +0800

    update add server

--- src/main/java/com/stu/util/StringUtil.java
+++ src/main/java/com/stu/util/StringUtil.java
@@ -2,4 +2,11 @@ package com.stu.util;
 
 public class StringUtil {
 
+	public static int changeInt(String str) {
+		try {
+			return Integer.valueOf(str);
+		} catch (Exception e) {
+			return 0;
+		}
+	}
 }
